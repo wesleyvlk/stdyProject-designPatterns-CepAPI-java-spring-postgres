a51807f1a27239356c5e9e381523af5edaf7667a
wesleyvlk <wesleymuniz20@gmail.com>
2023-01-29 11:08:55 -0300

    first commit

---
+++ stdyproject-cep_api-spring/src/main/java/com/dio/stdyprojectcep_apispring/StdyprojectCepApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dio.stdyprojectcep_apispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdyprojectCepApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdyprojectCepApiSpringApplication.class, args);
+	}
+
+}
